5ebd3db057e546116ba75284a95ba37baa25a9cd
Tim Huegdon <tim@nefariousdesigns.co.uk>
2011-12-09 15:51:33 +0000

    Made tests a bit more strict.

--- tests/test.bfchrome.inplayfootball.js
+++ tests/test.bfchrome.inplayfootball.js
@@ -1,76 +1,76 @@
 (function(){
     module('BFChrome.InPlayFootball');
 
-    test('zeroFill - Success', function(){
+    test('zeroFill - Successfully zero fills', function(){
         var value;
 
         expect(4);
 
-        equals(
+        strictEqual(
             BFChrome.InPlayFootball.zeroFill(1, 10),
             (value = '0000000001'),
             'Expecting value to be ' + value
         );
 
-        equals(
+        strictEqual(
             BFChrome.InPlayFootball.zeroFill(123, 10),
             (value = '0000000123'),
             'Expecting value to be ' + value
         );
 
-        equals(
+        strictEqual(
             BFChrome.InPlayFootball.zeroFill(41241, 10),
             (value = '0000041241'),
             'Expecting value to be ' + value
         );
 
-        equals(
+        strictEqual(
             BFChrome.InPlayFootball.zeroFill(1234567890, 10),
             (value = '1234567890'),
             'Expecting value to be ' + value
         );
     });
 
-    test('zeroFill - Returns String', function(){
+    test('zeroFill - Returns a string', function(){
         expect(2);
 
-        equals(
+        strictEqual(
             typeof(BFChrome.InPlayFootball.zeroFill(123, 10)),
             'string',
             'Expecting return type to be "string"'
         );
 
-        equals(
+        strictEqual(
             typeof(BFChrome.InPlayFootball.zeroFill(123, 3)),
             'string',
             'Expecting return type to be "string"'
         );
     });
 
-    test('zeroFill - Return length', function(){
+    test('zeroFill - Returns a string of the correct length', function(){
         var value;
 
         expect(4);
 
-        equals(
-            (value = 10),
+        strictEqual(
+            (value = 3),
             BFChrome.InPlayFootball.zeroFill(123, value).length,
             'Expecting length to be ' + value
         );
 
-        equals(
-            (value = 6),
+        strictEqual(
+            (value = 9),
             BFChrome.InPlayFootball.zeroFill(123, value).length,
             'Expecting length to be ' + value
         );
 
-        equals(
+        strictEqual(
             (value = 86),
             BFChrome.InPlayFootball.zeroFill(123, value).length,
             'Expecting length to be ' + value
         );
 
-        equals(
+        strictEqual(
             (value = 321),
             BFChrome.InPlayFootball.zeroFill(123, value).length,
             'Expecting length to be ' + value
